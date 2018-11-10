933dd3f69e1256d8f97d47a41abbecc7de22ac3d
Martins-Okuonghae <martins2018.okuonghae@outlook.com>
2018-11-10 01:34:13 +0000

    Dictionary Commit 10-11-2018

---
+++ dictionary.py
@@ -0,0 +1,37 @@
+# Defining a dictionary
+# Accessing Values in Dictionary
+
+dict_1 = {'Name':'Zara', 'Age':'7', 'Class':'First'}
+
+print(dict_1)
+
+print("dictionary 1 ['Age'] = ", dict_1['Age'])
+
+print("dictionary 1 ['Name'] is ", dict_1['Name'])
+
+print("ditionary 1 ['Class'] is ", dict_1['Class'])
+
+# Updating Dictionary
+
+dict_1['Age'] = 23
+
+dict_1['Status'] = 'Married'
+
+dict_1['School'] = 'DBS'
+
+
+print(dict_1)
+
+# Delete Dictionary Elements
+
+del dict_1['Name']
+
+dict_1.clear()
+
+#del dict_1
+
+dict_2 = {('First Name', 'Last Name'): 'Martins  ' 'Okuonghae ', 'Age':43}
+
+print(dict_1)
+
+print(dict_2)
